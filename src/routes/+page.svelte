35cbf995804e258935ecd29e18eae183d92a5867
Kevin Little <crispiestsquid@aol.com>
2024-01-01 16:43:37 -0500

    playing around on the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let count = 0;
+
+	$: doubled = count * 2;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<h1>This is my home page!</h1>
+<p>Count: <span id="count">{count}</span></p>
+<button on:click={handleClick}>Increment count</button>
+{#if count > 10}
+	<p>Count is really big!</p>
+	<p>If you double it you would have {doubled}</p>
+{/if}
+
+<style>
+	h1 {
+		color: red;
+	}
+	#count {
+		color: blue;
+	}
+
+	button {
+		background-color: #eee;
+	}
+</style>
