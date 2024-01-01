9b83e50bd8dc7114e816fd4951995b1355252b4b
Kevin Little <crispiestsquid@aol.com>
2024-01-01 17:44:00 -0500

    Updating the nav bar styles and adding active page indicator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,20 @@
+<script>
+	import { page } from '$app/stores';
+
+	$: pathname = $page.url.pathname;
+</script>
+
 <svelte:head>
 	<title>Kevin Little Tech</title>
 </svelte:head>
 
 <div id="nav-bar">
-	<p>{'<kevinlittle.tech />'}</p>
+	<a href="/"><p>{'<kevinlittle.tech />'}</p></a>
 	<nav>
-		<a href="/">Home</a>
-		<a href="/about">About</a>
-		<a href="/blog">Blog</a>
-		<a href="/login">Log In</a>
+		<a href="/" class:active={pathname === '/'}>Home</a>
+		<a href="/about" class:active={pathname === '/about'}>About</a>
+		<a href="/blog" class:active={pathname.includes('/blog')}>Blog</a>
+		<a href="/login" class:active={pathname === '/login'}>Log In</a>
 	</nav>
 </div>
 
@@ -30,17 +36,32 @@
 		padding: 1rem;
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
+	}
+
+	#nav-bar a {
+		text-decoration: none;
+		color: inherit;
 	}
 
 	#nav-bar p {
 		font-weight: bold;
-		padding: 0px;
-		margin: 0px;
+		margin: 0;
+		cursor: pointer;
 	}
 
 	nav a {
 		color: #fff;
-		text-decoration: none;
 		margin-right: 1rem;
+		transition: color 0.3s;
+	}
+
+	nav a:hover,
+	nav a.active {
+		color: #0077cc;
+	}
+
+	nav a.active {
+		border-bottom: 2px solid #0077cc;
 	}
 </style>
